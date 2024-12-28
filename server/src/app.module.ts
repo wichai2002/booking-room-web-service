d79b83c5ac9b254616981933561bf3fe5a120735
kommongkhun wichai <kommongkhun.wichai@gmail.com>
2024-12-28 18:40:44 +0700

    new module booking

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
 import { TableModule } from './table/table.module';
 import { RoomModule } from './room/room.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, CompanyModule, TableModule, RoomModule],
+  imports: [PrismaModule, UserModule, CompanyModule, TableModule, RoomModule, BookingModule],
   controllers: [AppController],
   providers: [AppService],
 })
